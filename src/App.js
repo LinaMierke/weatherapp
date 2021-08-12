a906987d151bd6c21bbb2a590da1c25c88330edf
lina mierkE <lina_maria89@hotmail.com>
2021-08-11 20:25:03 -0400

    app js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 import Search from "../src/components/Search/Search";
 import Weather from "../src/components/weather/Weather";
@@ -6,40 +7,43 @@ import "./App.css";
 
 export default function App() {
   const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-  const [weatherData, setWeatherData] = useState([]);
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const fetchWeatherData = (searchValue = null) => {
     //if is a zip run zip or q for value
     const usezipparam =
       searchValue.length === 5 ? `zip=${searchValue}` : `q=${searchValue}`;
     let URL = `https://api.openweathermap.org/data/2.5/forecast?&cnt=7&units=imperial&${usezipparam}&appid=${API_KEY}`;
-    fetch(URL) 
+
+    fetch(URL)
       .then((response) => response.json())
-      .then((jsonData) => setWeatherData(jsonData));
+      .then((jsonData) => setWeatherData(jsonData))
+      .catch((error) => {
+        console.log("Error fetching data: ", error);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   useEffect(() => {
     fetchWeatherData("22314");
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []);
+  if (loading) return "Loading...";
+  if (error) return "Error!...";
+  console.log("hola:", weatherData);
   return (
     <div className="main">
-      <div
-        className="title"
-        style={{ fontSize: "4em", fontWeight: "550", fontFamily: "Patra" }}
-      >
-        <a
-          href="https://movesfinancial.com/"
-          target="_blank"
-          rel="noreferrer"
-          style={{ textDecoration: "none", color: "black" }}
-        >
-          weather moves
-        </a>
+      <div className="title" style={{ fontSize: "4em" }}>
+        <p> weather moves </p>
       </div>
-      <div className="content">
-        <Search fetchWeatherData={fetchWeatherData} />
-        <Weather title={"Current Weather"} weatherData={weatherData} />
-      </div>
+      <div className="shape"></div>
+
+      <Search setWeatherData={weatherData} />
+      <Weather title={"Current Weather"} weatherData={fetchWeatherData} />
     </div>
   );
 }
